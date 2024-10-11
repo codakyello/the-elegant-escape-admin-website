9affeb4f88948b4addc4bec8546b8cf51c2b15f1
Codakyello <olaoluwathompson8@gmail.com>
2024-10-11 18:38:54 +0100

    layout

--- app/_components/SideBar.tsx
+++ app/_components/SideBar.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export default function SideBar() {
   return (
-    <aside className="fixed top-0 left-0 h-full flex py-[1.2rem] md:py-[3.2rem] md:px-[2.4rem] flex-col gap-7 md:gap-14 row-span-2">
+    <aside className=" fixed top-0 left-0 h-full flex py-[1.2rem] md:py-[3.2rem] md:px-[2.4rem] flex-col gap-7 md:gap-14 row-span-2 z-30">
       <Logo />
       <Nav />
     </aside>
