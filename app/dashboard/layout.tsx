aefbb46f56b664521ded3817f4b2accf8c6ad1c6
Codakyello <olaoluwathompson8@gmail.com>
2024-10-11 18:48:09 +0100

    layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ function Page({ children }: { children: ReactNode }) {
         <SideBar />
 
         <Header />
-        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-6rem)] md:w-[calc(100vw-22rem)] mt-[6rem] h-[calc(100vh-15em)] bg-[var(--color-grey-0)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
+        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-6rem)] md:w-[calc(100vw-22rem)] mt-[6rem] h-[calc(100vh-15em)] bg-[var(--color-grey-500)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
           <div className="max-w-[120rem] mx-auto no-scroll overflow-auto">
             {children}
           </div>
