dd3f8ddc8015dce36321636b0f7747a726b374a3
Codakyello <olaoluwathompson8@gmail.com>
2024-10-11 18:26:22 +0100

    layout

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import HeaderMenu from "@/app/_components/HeaderMenu";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-[26rem] right-0 h-[7rem] z-10 flex items-center gap-10 justify-end py-[1.2rem] px-[2rem] ">
+    <header className="fixed top-0 left-0 md:left-[26rem] bg-[var(--color-grey-0)] right-0 h-[7rem] z-10 flex items-center gap-10 justify-end py-[1.2rem] px-[2rem] ">
       <UserAvatar />
       <HeaderMenu />
     </header>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ function Page({ children }: { children: ReactNode }) {
         <SideBar />
 
         <Header />
-        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-9rem)] md:w-[calc(100vw-24rem)] mt-[6rem] h-[calc(100vh-4rem)] bg-[var(--color-grey-50)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
+        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-9rem)] md:w-[calc(100vw-24rem)] mt-[6rem] h-[calc(100vh-6rem)] bg-[var(--color-grey-50)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
           <div className="max-w-[120rem] mx-auto no-scroll overflow-auto">
             {children}
           </div>
