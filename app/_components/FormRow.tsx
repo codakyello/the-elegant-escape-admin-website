2351f12bb2bf6b246619d3dfec6291fd630ff573
Codakyello <olaoluwathompson8@gmail.com>
2024-10-11 18:16:04 +0100

    layout

--- app/_components/FormRow.tsx
+++ app/_components/FormRow.tsx
@@ -16,7 +16,7 @@ function FormRow({
       className={`mb-5 grid gap-3 grid-cols-1  ${
         orientation === "vertical"
           ? "md:gap-[.8rem]"
-          : "md:grid-cols-[24rem_.45fr] md:gap-[2.4rem] items-center"
+          : "md:grid-cols-[18.7rem_1fr] lg:grid-cols-[24rem_.5fr] md:gap-[2.4rem] items-center"
       }  md:py-[1.2rem] border-b border-[var(--color-grey-100)]`}
     >
       <label className={`font-medium`} htmlFor={htmlFor}>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,12 +7,14 @@ import { ReactNode } from "react";
 function Page({ children }: { children: ReactNode }) {
   return (
     <ProtectedRoute>
-      <Box className="grid h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[26rem_1fr]">
+      <Box className="grid h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[2rem_1fr]">
         <SideBar />
 
         <Header />
-        <main className="bg-[var(--color-grey-50)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
-          <div className="max-w-[120rem] mx-auto ">{children}</div>
+        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-9rem)] md:w-[calc(100vw-24rem)] mt-[6rem] h-[calc(100vh-4rem)] bg-[var(--color-grey-50)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
+          <div className="max-w-[120rem] mx-auto no-scroll overflow-auto">
+            {children}
+          </div>
         </main>
       </Box>
     </ProtectedRoute>
