09767ba1326f31dea07cc192f7947de483649e63
Codakyello <olaoluwathompson8@gmail.com>
2024-10-11 18:50:55 +0100

    layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,11 +7,11 @@ import { ReactNode } from "react";
 function Page({ children }: { children: ReactNode }) {
   return (
     <ProtectedRoute>
-      <Box className="grid h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[2rem_1fr]">
+      <Box className="grid min-h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[2rem_1fr]">
         <SideBar />
 
         <Header />
-        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-6rem)] md:w-[calc(100vw-22rem)] mt-[6rem] h-[calc(100vh-15em)] bg-[var(--color-grey-500)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
+        <main className="md:ml-[22rem] ml-[6rem] w-[calc(100vw-6rem)] md:w-[calc(100vw-22rem)] mt-[6rem] h-[calc(100vh-15em)] bg-[var(--color-grey-50)] p-5 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
           <div className="max-w-[120rem] mx-auto no-scroll overflow-auto">
             {children}
           </div>
