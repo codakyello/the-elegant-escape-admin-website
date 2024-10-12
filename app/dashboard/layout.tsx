e26b1b9714b9778f965d6050d8b4e86ac561ef74
Codakyello <olaoluwathompson8@gmail.com>
2024-10-12 19:18:16 +0100

    finish image upload and user update

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ function Page({ children }: { children: ReactNode }) {
       <Box className="grid min-h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[2rem_1fr]">
         <SideBar />
         <Header />
-        <main className="md:ml-[23rem] ml-[4.9rem] w-[calc(100vw-6rem)] md:w-[calc(100vw-23rem)] mt-[6rem] h-[calc(100vh-7rem)] bg-[var(--color-grey-50)] px-6 pt-14 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
+        <main className="md:ml-[23rem] ml-[4.9rem] w-[calc(100vw-8rem)] md:w-[calc(100vw-22rem)] mt-[6rem] h-[calc(100vh-7rem)] bg-[var(--color-grey-50)] px-6 pt-14 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
           <div className="max-w-[120rem] mx-auto no-scroll overflow-auto">
             {children}
           </div>
