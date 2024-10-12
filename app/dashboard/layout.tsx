018c8de9a3e8e99c0a1e6967f4d643c666db6697
Codakyello <olaoluwathompson8@gmail.com>
2024-10-12 19:33:40 +0100

    finish image upload and user update

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ import { ReactNode } from "react";
 function Page({ children }: { children: ReactNode }) {
   return (
     <ProtectedRoute>
-      <Box className="grid min-h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[2rem_1fr]">
+      <Box className="grid overflow-x-hidden min-h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] md:grid-cols-[2rem_1fr]">
         <SideBar />
         <Header />
         <main className="md:ml-[23rem] ml-[5rem] w-[calc(100vw-5rem)] md:w-[calc(100vw-22rem)] mt-[6rem] min-h-[calc(100vh-7rem)] bg-[var(--color-grey-50)] px-6 pt-14 md:pt-[4rem] md:pb-[6.4rem] md:px-[4.8rem]">
