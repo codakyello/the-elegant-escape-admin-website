2faf8227d67175f0ab04d7621dbf44fcad18364b
Codakyello <olaoluwathompson8@gmail.com>
2024-11-14 17:58:27 +0100

    mobile nav

--- app/_components/SideBar.tsx
+++ app/_components/SideBar.tsx
@@ -9,6 +9,8 @@ export default function SideBar() {
         <Logo />
         <Nav />
       </aside>
+
+      {/*Desktop Nav*/}
       <aside className="hidden bg-[var(--color-grey-0)] md:fixed w-[23rem] border-r border-r-[var(--color-grey-100)] top-0 left-0 h-full md:flex py-[1.2rem] md:py-[3.2rem] md:px-[2.4rem] items-center flex-col gap-16 row-span-2">
         <Logo />
         <Nav />
