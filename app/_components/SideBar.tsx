344ae47ce03e9f9d49d041ea13fe2c06f29d19b1
Codakyello <olaoluwathompson8@gmail.com>
2024-11-14 17:57:53 +0100

    mobile nav

--- app/_components/SideBar.tsx
+++ app/_components/SideBar.tsx
@@ -5,11 +5,11 @@ export default function SideBar() {
   return (
     <>
       {/*Mobile Nav*/}
-      <aside className="mobile-nav md:hidden bg-[var(--color-grey-0)] md:fixed w-[23rem] fixed border-r border-r-[var(--color-grey-100)] top-0 left-0 h-full flex py-[1.2rem] md:py-[3.2rem] md:px-[2.4rem]  flex-col gap-16 row-span-2 z-[9999]">
+      <aside className="mobile-nav md:hidden bg-[var(--color-grey-0)] w-[30rem] fixed border-r border-r-[var(--color-grey-100)] top-0 left-0 h-full flex py-[1.2rem] md:py-[3.2rem] flex-col gap-16 row-span-2 z-[9999]">
         <Logo />
         <Nav />
       </aside>
-      <aside className="hidden bg-[var(--color-grey-0)] md:fixed w-[23rem] border-r border-r-[var(--color-grey-100)] top-0 left-0 h-full md:flex py-[1.2rem] md:py-[3.2rem] md:px-[2.4rem] items-center flex-col gap-16  row-span-2 z-[9999]">
+      <aside className="hidden bg-[var(--color-grey-0)] md:fixed w-[23rem] border-r border-r-[var(--color-grey-100)] top-0 left-0 h-full md:flex py-[1.2rem] md:py-[3.2rem] md:px-[2.4rem] items-center flex-col gap-16 row-span-2">
         <Logo />
         <Nav />
       </aside>
